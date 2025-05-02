0f8a555e8b3f7038462de80046a4e6a983c2c34c
avneet16m <avneet200105@gmail.com>
2025-05-02 10:38:05 +0530

    added function

---
+++ avneet.py
@@ -0,0 +1,11 @@
+# Two number sum program in Python
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate sum
+total = num1 + num2
+
+# Display result
+print("The sum is:", total)
