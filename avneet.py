7446d1ff3338eb5b1a781f69279cb7c30db739ad
avneet16m <avneet200105@gmail.com>
2025-05-10 09:58:39 +0530

    Au task:

--- avneet.py
+++ avneet.py
@@ -9,3 +9,13 @@ total = num1 + num2
 
 # Display result
 print("The sum is:", total)
+#####
+def fib (n):
+    if n==0 or  n==1:
+        return  1
+    else:
+        return fib (n-1)+fib(n-2)
+    ###
+    def star(n):
+        for  i in range (1,n):
+            print(i"*")
